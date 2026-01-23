034a58a7dacfd3f00c4e43a9741f1925cda177ca
rthaqori <ramanthaqori@gmail.com>
2026-01-23 19:22:09 +0545

    fix

--- src/app/(USER_PAGES)/checkout/page.tsx
+++ src/app/(USER_PAGES)/checkout/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 export const dynamic = "force-dynamic";
-("use client");
 
 import { PlaceOrderButton } from "@/components/placeOrderButton";
 import { CartSummarySkeleton } from "@/components/skeletons/cartSummarySkeleton";
